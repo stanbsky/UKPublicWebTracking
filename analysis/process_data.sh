bd45abaaad3473c637f04bce178c4474d47f147d
Stan Borzhemsky <stanbsky@protonmail.com>
2021-12-30 22:02:47 +0000

    Add handing of both precrawl and crawl files

--- analysis/process_data.sh
+++ analysis/process_data.sh
@@ -9,16 +9,23 @@ PYTHON=$1
 # Name of crawl directory
 CRAWL=$2
 
+# Figure out if we're working with a crawl/precrawl
+if [ -f "data/$CRAWL/crawl.sqlite" ]; then
+    CTYPE=crawl
+else
+    CTYPE=precrawl
+fi
+
 # Log all python command output
 LOG=data/$CRAWL/process.log
 exec > >(tee -ia $LOG) 2>&1
 
-DB=data/$CRAWL/precrawl.sqlite
+DB=data/$CRAWL/$CTYPE.sqlite
 REDIRECTS=data/$CRAWL/redirects.json
 
 # Run all processing scripts
 $PYTHON analysis/pair_responses_and_requests.py $DB
-$PYTHON analysis/merge_leveldb_data.py $DB data/$CRAWL/precrawl-leveldb
+$PYTHON analysis/merge_leveldb_data.py $DB data/$CRAWL/$CTYPE-leveldb
 $PYTHON analysis/record_visit_redirects.py $DB $REDIRECTS
 $PYTHON analysis/record_categories.py $DB crawl/lists/urls.json
 $PYTHON analysis/parse_request_urls.py $DB crawl/lists/disconnect.json
--- gather_data.sh
+++ gather_data.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# Figure out if we're working with a crawl/precrawl
+if [ -f "data/$1/crawl.sqlite" ]; then
+    CTYPE=crawl
+else
+    CTYPE=precrawl
+fi
+
 source analysis/utilities.sh
-echo "Renaming and moving directories..."
+echo "Deleting intermediate screenshots..."
 rm -rf data/$1/screenshots/parts
+echo "Moving screenshot folder..."
 mv data/$1/screenshots data/$1-screenshots
-mv logs/precrawl.log data/$1/$1.log
+echo "Moving profiles folder..."
+mv data/$1/profiles data/$1-profiles
+echo "Moving logs..."
+mv logs/$CTYPE.log data/$1/$1.log
+mv logs/$CTYPE-openwpm.log data/$1/$1-openwpm.log
 cd data
-echo "Uploading logs..."
-pushd $1
-upload_to_gdrive $1.log 2
-rm $1.tar.xz
-popd
 echo "Uploading crawl data..."
-upload_to_gdrive $1 10
+upload_to_gdrive $1 15
