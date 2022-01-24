d74f00cee9b0882445fc2f3357180a6f44061bd4
Stan Borzhemsky <stanbsky@protonmail.com>
2022-01-24 00:43:52 +0000

    Improve attribution approval script to label multiple sites if possible

--- analysis/approve_attributions.py
+++ analysis/approve_attributions.py
@@ -4,44 +4,68 @@ import sqlite3
 from contextlib import closing
 from json import loads
 from pprint import pprint as pp
+from tldextract import extract
+
+def check_for_more(con, attrib):
+    others = con.execute(f"SELECT rowid FROM attrib WHERE attrib_data LIKE '%\"href\": \"{attrib}\"%' AND attrib_url IS NULL").fetchall()
+    sites_updated = len(others)
+    if others:
+        for row in others:
+            commit(con, (attrib, row[0]))
+    return sites_updated
 
 def commit(con, commit_data):
-    con.executemany('UPDATE attrib SET attrib_url = ? WHERE rowid = ?', commit_data)
+    con.execute('UPDATE attrib SET attrib_url = ? WHERE rowid = ?', commit_data)
     con.commit()
 
-if __name__ == "__main__":
-    with closing(sqlite3.connect(sys.argv[1])) as con:
+def hr():
+    print(80*'-')
+
+def approve_attributions(db):
+    with closing(sqlite3.connect(db)) as con:
         with closing(con.cursor()) as cur:
             con.row_factory = sqlite3.Row
-            commit_data = []
-            bad_hrefs = []
-            good_hrefs = []
+            bad_candidates = ['twitter.com','t.co','youtube.com','vimeo.com','google.com','google.co.uk','facebook.com']
             rows = con.execute("SELECT rowid, url, attrib_data FROM attrib WHERE attrib_url IS NULL AND attrib_data IS NOT NULL").fetchall()
+            rows_left = len(rows)
+            last_update = 0
             for row in rows:
                 os.system('clear')
-                print('Accepted candidates:\n')
-                print(commit_data, '\n', '-'*16, '\n')
-
+                if last_update:
+                    print(f'{last_update} site attributions written.\n')
+                print(f'{rows_left} websites are awaiting labelling.\n\n')
                 print(f"Site: {row['url']}\n")
                 data = loads(row['attrib_data'])
-                for idx, val in enumerate(data):
-                    if val['href'] in bad_hrefs:
+                candidates = []
+                for candidate in data:
+                    url = candidate['href']
+                    if url in bad_candidates:
+                        # The usual third-party links that are not attributions...
                         continue
-                    if val['href'] in good_hrefs:
-                        commit_data.append((data[vote-1]['href'], row['rowid']))
+                    if url in row['url']:
+                        # If we're here, find_attributions messed up...
                         continue
-                    print(f"Candidate {idx+1}:\n")
-                    pp(val)
-                vote = int(input('\nEnter candidate number to accept, or 0 if none are applicable: '))
-                if vote:
-                    commit_data.append((data[vote-1]['href'], row['rowid']))
-                    good_hrefs.append(data[vote-1]['href'])
+                    parts = extract(url)
+                    if not parts.domain or not parts.suffix:
+                        # This is not a proper url
+                        continue
+                    candidates.append(candidate)
+                    hr()
+                    print(f"Candidate {len(candidates)}:\n")
+                    pp(candidate)
+                if candidates:
+                    vote = int(input('\nEnter candidate number to accept, or 0 if none are applicable: '))
                 else:
-                    commit_data.append(('--', row['rowid']))
-                if len(commit_data) >= 100:
-                    # Process in batches of 100, just in case of issues
+                    vote = 0
+                if vote:
+                    good_attrib = data[vote-1]['href']
+                    # No need to commit, check_for_more will mark this one and others
+                    last_update = check_for_more(con, good_attrib)
+                    rows_left -= last_update
+                else:
+                    commit_data = ('--', row['rowid'])
                     commit(con, commit_data)
-                    commit_data = []
-            
-            commit(con, commit_data)
-            print(f'Updated attributions for {con.total_changes} websites')
+                    rows_left -= 1
+
+if __name__ == "__main__":
+    approve_attributions(sys.argv[1])
