9d1a986a2bf387c383f8e5b4c3e34aa83dbbda1b
Stan Borzhemsky <stanbsky@protonmail.com>
2022-02-22 23:04:34 +0000

    Update process data script with visit_id deduplication and other changes

--- analysis/process_data.sh
+++ analysis/process_data.sh
@@ -16,6 +16,13 @@ else
     CTYPE=precrawl
 fi
 
+# If it's a CMP crawl database, treat it differently
+if [ -z "$3" ]; then
+    CMP=
+else
+    CMP=--cmp
+fi
+
 # Log all python command output
 LOG=data/$CRAWL/process.log
 exec > >(tee -ia $LOG) 2>&1
@@ -24,13 +31,14 @@ DB=data/$CRAWL/$CTYPE.sqlite
 REDIRECTS=data/$CRAWL/redirects.json
 
 # Run all processing scripts
-$PYTHON analysis/pair_responses_and_requests.py $DB
+$PYTHON analysis/check_for_duplicate_visit_ids.py $DB
+$PYTHON analysis/pair_responses_and_requests.py $DB $CMP
 $PYTHON analysis/merge_leveldb_data.py $DB data/$CRAWL/$CTYPE-leveldb
 $PYTHON analysis/record_visit_redirects.py $DB $REDIRECTS
 $PYTHON analysis/record_categories.py $DB crawl/lists/urls.json
 $PYTHON analysis/parse_request_urls.py $DB crawl/lists/disconnect.json
 $PYTHON analysis/parse_logs.py data/$CRAWL/$CRAWL.log \
-    --db data/$CRAWL/parsed_log.sqlite --table $CRAWL --redirects $REDIRECTS
+    --db data/$CRAWL/parsed_log.sqlite --table $CRAWL 
 
 # Output time taken to process data
 duration=$SECONDS
