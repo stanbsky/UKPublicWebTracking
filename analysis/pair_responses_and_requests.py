8a4916c581d5911a966e86b10093414f01dc4979
Stan Borzhemsky <stanbsky@protonmail.com>
2022-02-22 22:54:41 +0000

    Add CMP-crawl awareness to pairing responses and requests

--- analysis/pair_responses_and_requests.py
+++ analysis/pair_responses_and_requests.py
@@ -1,24 +1,36 @@
-import sys
 import sqlite3
+import argparse
 from contextlib import closing
+from pathlib import Path
 
-with closing(sqlite3.connect(sys.argv[1])) as con:
-    with closing(con.cursor()) as cur:
-        cur.executescript("""
-            DROP TABLE IF EXISTS request_data;
-            CREATE TABLE request_data AS
-	        SELECT http_requests.id AS request_id,
-                http_responses.id AS response_id,
-                http_requests.visit_id,
-                http_requests.top_level_url AS url,
-                http_requests.method,
-                http_requests.headers AS request_headers,
-                http_responses.response_status,
-                http_responses.response_status_text,
-                http_responses.headers AS response_headers,
-                http_responses.content_hash
-                FROM http_requests INNER JOIN http_responses
-                ON http_requests.url = http_responses.url
-                WHERE http_requests.resource_type = 'main_frame'
-                GROUP BY http_requests.url;
-        """)
+
+def create_request_data(db, cmp):
+    with closing(sqlite3.connect(db)) as con:
+        cur = con.cursor()
+        if cmp:
+            # Separate out the vanilla crawl for building request_data
+            cur.executescript('''
+                CREATE TEMPORARY TABLE site_visits AS 
+                SELECT * FROM site_visits WHERE browser_id IN 
+                (SELECT browser_id FROM crawl WHERE browser_params NOT LIKE '%.tar%')
+                              ''')
+            pref = 'temp.'
+        else:
+            pref = ''
+        con.commit()
+
+        cur.executescript(
+            f"CREATE TEMPORARY TABLE chash AS "
+            f"SELECT content_hash, visit_id FROM http_responses GROUP BY visit_id;"
+            f"DROP TABLE IF EXISTS request_data;"
+            f"CREATE TABLE request_data AS "
+                f"SELECT {pref}site_visits.visit_id, site_url AS url, chash.content_hash "
+                f"FROM site_visits JOIN chash ON chash.visit_id = {pref}site_visits.visit_id"
+        )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Creates a request_data table pairing requests and responses for the main GET request used to navigate to a website.')
+    parser.add_argument('db', type=Path)
+    parser.add_argument('--cmp', action='store_true')
+    args = parser.parse_args()
+    create_request_data(args.db, args.cmp)
