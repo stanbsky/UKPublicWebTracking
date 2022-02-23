ae105d3c134d5326cbca2b95912558d607245a68
Stan Borzhemsky <stanbsky@protonmail.com>
2022-02-23 09:43:30 +0000

    Rewrite request-responses pairing for better matching main results

--- analysis/pair_responses_and_requests.py
+++ analysis/pair_responses_and_requests.py
@@ -1,3 +1,4 @@
+import logging
 import sqlite3
 import argparse
 from contextlib import closing
@@ -7,30 +8,44 @@ from pathlib import Path
 def create_request_data(db, cmp):
     with closing(sqlite3.connect(db)) as con:
         cur = con.cursor()
+
+        # Speed up this query and future analyses by building visit_id indices
+        tables = ['requests','responses']
+        for table in tables:
+            try:
+                logging.info(f'Adding index on visit_id in http_{table}...')
+                cur.execute(f'CREATE INDEX {table}_vid ON http_{table}(visit_id)')
+            except sqlite3.Error:
+                logging.warning(f'Index already exists')
+
+            query = '''
+        CREATE TABLE request_data AS
+        SELECT content_hash, http_responses.visit_id, http_responses.url FROM http_responses 
+        -- Attempt to make sure we are grabbing the content hash for the main page
+        INNER JOIN http_requests USING (visit_id, request_id) WHERE http_requests.resource_type = 'main_frame' 
+        '''
+
         if cmp:
             # Separate out the vanilla crawl for building request_data
-            cur.executescript('''
-                CREATE TEMPORARY TABLE site_visits AS 
-                SELECT * FROM site_visits WHERE browser_id IN 
-                (SELECT browser_id FROM crawl WHERE browser_params NOT LIKE '%.tar%')
-                              ''')
-            pref = 'temp.'
-        else:
-            pref = ''
-        con.commit()
+            query = query + "AND http_responses.browser_id IN (SELECT browser_id FROM crawl WHERE browser_params NOT LIKE '%accept%' AND browser_params NOT LIKE '%reject%')"
+        query = query + 'GROUP BY http_responses.visit_id'
 
-        cur.executescript(
-            f"CREATE TEMPORARY TABLE chash AS "
-            f"SELECT content_hash, visit_id FROM http_responses GROUP BY visit_id;"
-            f"DROP TABLE IF EXISTS request_data;"
-            f"CREATE TABLE request_data AS "
-                f"SELECT {pref}site_visits.visit_id, site_url AS url, chash.content_hash "
-                f"FROM site_visits JOIN chash ON chash.visit_id = {pref}site_visits.visit_id"
-        )
+        logging.info('Finding main page content hashes...')
+        cur.execute(query)
+        con.commit()
+        entries_written = cur.execute('SELECT COUNT(*) FROM request_data').fetchall()[0][0]
+        logging.info(f'Done. {entries_written} entries written to request_data')
+
+        # Sanity check
+        null_hash_rows = cur.execute('SELECT COUNT(*) FROM request_data WHERE content_hash IS NULL').fetchall()[0][0]
+        if null_hash_rows:
+            logging.warning(f'No content_hash recorded for {null_hash_rows} rows!')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Creates a request_data table pairing requests and responses for the main GET request used to navigate to a website.')
     parser.add_argument('db', type=Path)
     parser.add_argument('--cmp', action='store_true')
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
     create_request_data(args.db, args.cmp)
